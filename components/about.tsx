3c278a1edf3c1d3999e36e1db90fba6e8ab76442
Ellis Chung <ellischung98@gmail.com>
2025-03-12 11:33:28 -0400

    edited about me section and updated resume + footer

--- components/about.tsx
+++ components/about.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
@@ -12,33 +11,24 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className="mb-28 max-w-[55rem] leading-8 scroll-mt-28 font-roboto-mono sm:mb-40"
+      className="mb-28 max-w-[55rem] leading-8 scroll-mt-28 font-roboto-mono sm:mb-40 text-center mx-auto"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
-      <div className="flex flex-col text-center items-center justify-center lg:flex-row lg:text-left">
-        <Image
-          src="/ellis-photo.jpg"
-          alt="ellis"
-          width="240"
-          height="240"
-          quality="95"
-          priority={true}
-          className="mb-8 h-24 w-24 rounded-full object-cover border-2 dark:border-white shadow-xl lg:mb-0 lg:mr-24 lg:h-60 lg:w-60"
-        />
+      <div className="max-w-[45rem] mx-auto">
         <div>
-          <p className="mb-3">
+          <p className="mb-5">
             My name is Ellis Chung and I am a graduate from Stony Brook
             University with a B.S. in{" "}
             <span className="underline">Computer Science</span>. I am currently
-            looking for a <span className="italic">full-time position</span> as
-            a software developer. I have experience in front-end, back-end, and
+            looking for growth opportunities as a software engineer / full-stack
+            developer. I have experience in front-end, back-end, and
             full-stack web development with my core tech stack being{" "}
             <span className="underline">
-              React, Next.js, Node.js, and MongoDB
+              TypeScript, Next.js, React, Node.js, Express, and PostgreSQL
             </span>
             . However, my passion lies in crafting visually stunning web designs
             so my main focus resides in{" "}
@@ -52,7 +42,7 @@ export default function About() {
             my other passions such as b-boying, building PCs, and learning about
             new tech. I also enjoy playing video games and watching anime. I
             love <span className="underline">learning new things</span> and I am
-            currently trying to get better at Japanese. 頑張ります!
+            currently trying to improve my Japanese skills. 頑張ります!
           </p>
         </div>
       </div>
--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-8 px-4 text-center font-roboto-mono text-gray-500">
       <small className="text-xs">
-        &copy; 2023 Ellis Chung. All rights reserved.
+        &copy; 2025 Ellis Chung. All rights reserved.
       </small>
     </footer>
   );
